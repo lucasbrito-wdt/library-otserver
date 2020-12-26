c79aa14f29f49862f4362e44d021eb5bffa0c4b7
Web Design Technologies <luquinhasbritogba@hotmail.com>
2020-12-26 14:28:02 -0300

    Coreção de Bugs

--- src/groups.php
+++ src/groups.php
@@ -1,64 +1,77 @@
 <?php
-  namespace Otserver;
 
-  class Groups implements Iterator, Countable {
+namespace Otserver;
+
+use Iterator;
+use Countable;
+
+class Groups implements Iterator, Countable
+{
 
     private $groups = array();
     public $iterator = 0;
 
-    public function __construct() {
-      foreach (Website::getDBHandle()->query('SELECT * FROM ' . Website::getDBHandle()->tableName('groups')) as $group) {
-        $groupData = array();
-        $groupData['id'] = $group['id'];
-        $groupData['name'] = $group['name'];
-        $this->groups[$groupData['id']] = new Group($groupData);
-      }
+    public function __construct()
+    {
+        foreach (Website::getDBHandle()->query('SELECT * FROM ' . Website::getDBHandle()->tableName('groups')) as $group) {
+            $groupData = array();
+            $groupData['id'] = $group['id'];
+            $groupData['name'] = $group['name'];
+            $this->groups[$groupData['id']] = new Group($groupData);
+        }
     }
 
     /*
      * Get group
      */
 
-    public function getGroup($id) {
-      if (isset($this->groups[$id]))
-        return $this->groups[$id];
+    public function getGroup($id)
+    {
+        if (isset($this->groups[$id]))
+            return $this->groups[$id];
 
-      return false;
+        return false;
     }
 
     /*
      * Get group name without getting group
      */
 
-    public function getGroupName($id) {
-      if ($group = self::getGroup($id))
-        return $group->getName();
+    public function getGroupName($id)
+    {
+        if ($group = self::getGroup($id))
+            return $group->getName();
 
-      return false;
+        return false;
     }
 
-    public function current() {
-      return $this->groups[$this->iterator];
+    public function current()
+    {
+        return $this->groups[$this->iterator];
     }
 
-    public function rewind() {
-      $this->iterator = 0;
+    public function rewind()
+    {
+        $this->iterator = 0;
     }
 
-    public function next() {
-      ++$this->iterator;
+    public function next()
+    {
+        ++$this->iterator;
     }
 
-    public function key() {
-      return $this->iterator;
+    public function key()
+    {
+        return $this->iterator;
     }
 
-    public function valid() {
-      return isset($this->groups[$this->iterator]);
+    public function valid()
+    {
+        return isset($this->groups[$this->iterator]);
     }
 
-    public function count() {
-      return count($this->groups);
+    public function count()
+    {
+        return count($this->groups);
     }
-
-  }
+}
--- src/visitor.php
+++ src/visitor.php
@@ -1,8 +1,11 @@
 <?php
-  namespace Otserver;
 
-  class Visitor {
+namespace Otserver;
 
+use Illuminate\Support\Facades\Session;
+
+class Visitor extends Session
+{
     const LOGINSTATE_NOT_TRIED = 1;
     const LOGINSTATE_NO_ACCOUNT = 2;
     const LOGINSTATE_WRONG_PASSWORD = 3;
@@ -13,67 +16,112 @@
     private static $account;
     private static $loginState = self::LOGINSTATE_NOT_TRIED;
 
-    public static function setAccount($value) {
-      $_SESSION['account'] = $value;
+    public function __construct()
+    {
+        self::login();
     }
 
-    public static function setPassword($value) {
-      $_SESSION['password'] = $value;
+    public static function setAccount($value)
+    {
+        Session::put(['account' => $value]);
     }
 
-    public static function getAccount() {
-      if (!isset(self::$account))
-        self::loadAccount();
-      return self::$account;
+    public static function setPassword($value)
+    {
+        Session::put(['password' => $value]);
     }
 
-    public static function loadAccount() {
-      self::$account = new Account();
-      if (!empty(self::$loginAccount)) {
-        self::$account->loadByName(self::$loginAccount);
-        if (self::$account->isLoaded())
-          if (self::$account->isValidPassword(self::$loginPassword))
-            self::$loginState = self::LOGINSTATE_LOGGED;
-          else
-            self::$loginState = self::LOGINSTATE_WRONG_PASSWORD;
-        else
-          self::$loginState = self::LOGINSTATE_NO_ACCOUNT;
-      } else
-        self::$loginState = self::LOGINSTATE_NOT_TRIED;
-      if (self::$loginState != self::LOGINSTATE_LOGGED)
+    public static function setAccountLoaded($account)
+    {
+        Session::put(['accountLogged' => $account]);
+    }
+
+    public static function setLoginState($state)
+    {
+        Session::put(['loginState' => $state]);
+    }
+
+    public static function getAccount()
+    {
+        if (Session::has('accountLogged'))
+            self::$account = Session::get('accountLogged');
+        if (!isset(self::$account))
+            self::loadAccount();
+        return self::$account;
+    }
+
+    public static function loadAccount()
+    {
         self::$account = new Account();
+        if (!empty(self::$loginAccount)) {
+            self::$account->loadByName(self::$loginAccount);
+            if (self::$account->isLoaded())
+                if (self::$account->isValidPassword(self::$loginPassword)) {
+                    self::$loginState = self::LOGINSTATE_LOGGED;
+
+                    self::setAccountLoaded(self::$account);
+                    self::setLoginState(self::$loginState);
+                } else {
+                    self::$loginState = self::LOGINSTATE_WRONG_PASSWORD;
+                    self::setLoginState(self::$loginState);
+                }
+            else {
+                self::$loginState = self::LOGINSTATE_NO_ACCOUNT;
+                self::setLoginState(self::$loginState);
+            }
+        } else {
+            self::$loginState = self::LOGINSTATE_NOT_TRIED;
+            self::setLoginState(self::$loginState);
+        }
+
+        if (self::$loginState != self::LOGINSTATE_LOGGED)
+            self::$account = new Account();
     }
 
-    public static function isTryingToLogin() {
-      return !empty(self::$loginAccount);
+    public static function isTryingToLogin()
+    {
+        if (Session::has('account'))
+            self::$loginAccount = Session::get('account');
+        return !empty(self::$loginAccount);
     }
 
-    public static function getLoginState() {
-      return self::$loginState;
+    public static function getLoginState()
+    {
+        if (Session::has('loginState'))
+            self::$loginState = Session::get('loginState');
+
+        return self::$loginState;
     }
 
-    public static function isLogged() {
-      return self::isTryingToLogin() && self::getAccount()->isLoaded();
+    public static function isLogged()
+    {
+        return self::isTryingToLogin() && Session::has('accountLogged');
     }
 
-    public static function login() {
-      if (isset($_SESSION['account']))
-        self::$loginAccount = $_SESSION['account'];
-      if (isset($_SESSION['password']))
-        self::$loginPassword = $_SESSION['password'];
+    public static function login()
+    {
+        if (Session::has('account'))
+            self::$loginAccount = Session::get('account');
+        if (Session::has('password'))
+            self::$loginPassword = Session::get('password');
+        return self::getAccount();
     }
 
-    public static function logout() {
-      unset($_SESSION['account']);
-      unset($_SESSION['password']);
-      self::$loginAccount = null;
-      self::$loginPassword = null;
-      self::$account = new Account();
-      self::$loginState = self::LOGINSTATE_NOT_TRIED;
+    public static function logout()
+    {
+        Session::remove('account');
+        Session::remove('password');
+        Session::remove('accountLogged');
+        Session::remove('loginState');
+
+        self::$loginAccount = null;
+        self::$loginPassword = null;
+        self::$account = new Account();
+        self::$loginState = self::LOGINSTATE_NOT_TRIED;
     }
 
-    public static function getIP() {
-      return ip2long($_SERVER['REMOTE_ADDR']);
+    public static function getIP()
+    {
+        return ip2long($_SERVER['REMOTE_ADDR']);
     }
-
-  }
+}
